c319e8d2b7748279684873e3f0559b6881ebf5d3
unknown-SC <unknownsecret112001@gmail.com>
2026-04-19 19:45:23 +0700

    ubah urutan def terus variabelnya

--- konversiskalabacaan.py
+++ konversiskalabacaan.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
-#gjgjgjgnvnjjjj
-#instal "pip install openpyxl" pada terminan atau diawal program
+
+#instal "pip install openpyxl" pada terminan atau diawal program buat baca xlsx
+
+def konversi (value_in_mGal, skala_bacaan, counter_reading, factor_for_interval):
+    return counter_reading + (skala_bacaan - value_in_mGal) * factor_for_interval
 
 df = pd.read_excel(r"C:\Users\VIVOBOOK\Documents\MBG\115230031_Data G-OBS.xlsx", usecols="E") #kasih alamat file excelnya yh
 
 df.iloc[:,0] = pd.to_numeric(df.iloc[:,0], errors="coerce")
 
-counter_reading = np.array([1800, 1700, 1600])
+counter_reading = np.array([1800, 1700, 1600])              
 value_in_mGal = np.array([1906.96, 1801.03, 1695.11])
 factor_for_interval = np.array([1.0593, 1.05929, 1.05925])
 
@@ -15,9 +18,6 @@ skala_bacaan = df.iloc[:,0].to_numpy().reshape(-1,1)
 skala_bacaan = skala_bacaan[(skala_bacaan >= 1700) & (skala_bacaan <= 1800)]
 skala_bacaan = skala_bacaan.reshape(-1,1)
 print("Nilai Skala Bacaan\n", skala_bacaan)
-
-def konversi (value_in_mGal, skala_bacaan, counter_reading, factor_for_interval):
-    return counter_reading + (skala_bacaan - value_in_mGal) * factor_for_interval
 
 konversi_skala_bacaan = []
 
--- test.konversi.py
+++ test.konversi.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from konversi_skala_bacaan import konversi #from 
+from konversi_skala_bacaan import konversi #from nama file import nama fungsinya
 
 def konversi (value_in_mGal, skala_bacaan, counter_reading, factor_for_interval):
     return counter_reading + (skala_bacaan - value_in_mGal) * factor_for_interval
